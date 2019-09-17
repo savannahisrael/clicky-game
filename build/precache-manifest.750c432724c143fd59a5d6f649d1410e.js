acf01b315f9bcb7e6b7ac17f694efdd91e2c2ae8
Savannah Israel <sisrael0405@gmail.com>
2019-09-17 14:20:43 -0600

    deployed page working

---
+++ build/precache-manifest.750c432724c143fd59a5d6f649d1410e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7674f027476fd4a9412f7137cad657d5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c269035f6e4c67ac69bd",
+    "url": "/clicky-game/static/css/main.23d8330b.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/clicky-game/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "c269035f6e4c67ac69bd",
+    "url": "/clicky-game/static/js/main.39830231.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
